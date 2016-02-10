c342c43e99d1a10fb4ddbd31d4389a9fc5d54e7e
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-02-10 13:38:45 +0100

    Doc update

--- mtd-rw.c
+++ mtd-rw.c
@@ -22,6 +22,14 @@
 #include <linux/mtd/mtd.h>
 
 /*
+ * This module is intended for embedded devices where the mtd partition
+ * layout may be hard-coded in the firmware. If, for some reason, you
+ * DO have to write to a read-only partition (which is often a bad idea),
+ * this module is the way to go.
+ *
+ * The module is currently limited to the first 64 partitions, but this
+ * should suffice for most purposes.
+ *
  * Inspired by dougg3@electronics.stackexchange.com:
  * https://electronics.stackexchange.com/a/116133/97342
  */
